efca2e2b51811b83d2ba97f2d8ad71827d3cf5b0
Takashi IMAHIRO <imahiro@erin.jp>
2021-02-28 19:10:48 +0900

    fix

--- lib/lambda_base.ex
+++ lib/lambda_base.ex
@@ -13,16 +13,4 @@ defmodule LambdaBase do
   Lambda runtime call handle function.
   """
   @callback handle(event :: map(), context :: map()) :: {:ok, String.t} | {:error, String.t}
-
-  defmacro __using__(_opts) do
-    quote do
-      @behaviour LambdaBase
-      def start() do
-        context = System.get_env
-        HTTPoison.start()
-        {:ok, context} = init(context)
-        LambdaBase.Base.loop(context)
-      end
-    end
-  end
 end
